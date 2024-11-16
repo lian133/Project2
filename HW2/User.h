1939957cbff255faa5650898a90affdcca73f9e3
unknown <lianyy007@gmail.com>
2024-11-16 12:24:06 +0200

    User - h file

---
+++ HW2/User.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "DeviceList.h"
+#include <string>
+
+class User
+{
+private:
+	unsigned int id;
+	std::string username;
+	unsigned int age;
+	DevicesList devices;
+
+public:
+	void init(const unsigned int id,const std::string username, const unsigned int age);
+	void clear();
+	unsigned int getID() const;
+	std::string getUserName() const;
+	unsigned int getAge() const;
+	DevicesList& getDevices();
+	void addDevice(Device newDevice);
+	bool checkIfDevicesAreOn() const;
+};
